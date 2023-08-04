9cf355470127012b5462aa529915ed4e6a8f8205
Adrianova Rina <107701817+rinaadrianova@users.noreply.github.com>
2023-08-04 15:40:32 +0300

    попытка сделать домашку №2

--- src/channel.py
+++ src/channel.py
@@ -1,21 +1,24 @@
 import json
 import os
 from googleapiclient.discovery import build
+
 from helper.youtube_api_manual import channel_id
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """Класс для ютуб - канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API."""
         self.response = None
         self.channel_id = channel_id
 
         # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
         api_key: str = os.getenv('API_KEY')
 
-        # Эта строка создает объект youtube, который будет использоваться для взаимодействия с YouTube API.
+        # Эта строка создает объект youtube,
+        # который будет использоваться для взаимодействия с YouTube API.
         self.youtube = build('youtube', 'v3', developerKey=api_key)
 
     def print_info(self) -> None:
