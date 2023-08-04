0b447c89d8099792a8176238ba3dd3768c00f22c
Adrianova Rina <107701817+rinaadrianova@users.noreply.github.com>
2023-08-04 15:28:18 +0300

    попытка сделать домашку №1

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -8,7 +8,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('API_KEY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -39,7 +39,7 @@ playlists = youtube.playlists().list(channelId=channel_id,
                                      part='contentDetails,snippet',
                                      maxResults=50,
                                      ).execute()
-# printj(playlists)
+printj(playlists)
 for playlist in playlists['items']:
     print(playlist)
     print()
@@ -58,7 +58,7 @@ playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
                                                part='contentDetails',
                                                maxResults=50,
                                                ).execute()
-# printj(playlist_videos)
+printj(playlist_videos)
 
 # получить все id видеороликов из плейлиста
 video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
@@ -72,7 +72,7 @@ docs: https://developers.google.com/youtube/v3/docs/videos/list
 video_response = youtube.videos().list(part='contentDetails,statistics',
                                        id=','.join(video_ids)
                                        ).execute()
-# printj(video_response)
+printj(video_response)
 
 for video in video_response['items']:
     # YouTube video duration is in ISO 8601 format
@@ -90,7 +90,7 @@ video_id = 'gaoc9MPZ4bw'
 video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                        id=video_id
                                        ).execute()
-# printj(video_response)
+printj(video_response)
 video_title: str = video_response['items'][0]['snippet']['title']
 view_count: int = video_response['items'][0]['statistics']['viewCount']
 like_count: int = video_response['items'][0]['statistics']['likeCount']
--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,7 @@
+import json
+import os
+from googleapiclient.discovery import build
+from helper.youtube_api_manual import channel_id
 
 
 class Channel:
@@ -5,8 +9,18 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.response = None
+        self.channel_id = channel_id
+
+        # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+        api_key: str = os.getenv('API_KEY')
+
+        # Эта строка создает объект youtube, который будет использоваться для взаимодействия с YouTube API.
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+        self.response = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        channel_info = json.dumps(self.response, indent=2, ensure_ascii=False)
+        return channel_info
